35bb3201f42a59b9d97a4c85198ad92a918137ae
ifUsernameIsTooLongNobodyWantToSeeItAll <xhily_@outlook.com>
2024-07-10 17:32:32 +0800

    Finish Second Homework

--- 2/src/hook/useDebounce.tsx
+++ 2/src/hook/useDebounce.tsx
@@ -5,16 +5,13 @@ export default function useDebounce<T extends (...args: any[]) => any>(fn: T, de
 
     return useCallback(
         (...args: Parameters<T>): void => {
-            // 如果已经有定时器在运行，则清除它
             if (timer.current) {
                 clearTimeout(timer.current);
             }
 
             // 设置新的定时器
             timer.current = setTimeout(() => {
-                // 调用原始函数，确保 this 上下文正确（通常为 undefined 或 null）
                 fn(args)
-                // 清除定时器引用，防止内存泄漏
                 timer.current = undefined;
             }, delay);
         },[]
